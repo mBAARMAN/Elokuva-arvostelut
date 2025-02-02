3ff21ef1f7be78d4ea7426d04dc54a31b368bcbe
Baarman Mikael B <mbaarman@cubbli22-gold.local>
2025-02-02 15:58:09 +0200

    Show movie list/info

--- movies.py
+++ movies.py
@@ -3,3 +3,17 @@ import db
 def add_movie(title, director, year, description, user_id):
     sql = "INSERT INTO movies (title, director, year, description, user_id) VALUES (?, ?, ?, ?, ?)"
     db.execute(sql, [title, director, year, description, user_id])
+
+def get_movies():
+    sql = "SELECT id, title FROM movies ORDER BY id DESC"
+    return db.query(sql)
+
+def get_movie(movie_id):
+    sql = """SELECT movies.title,
+                    movies.director,
+                    movies.year,
+                    movies.description
+            FROM movies, users
+            WHERE movies.user_id = users.id AND
+                movies.id = ?"""
+    return db.query(sql, [movie_id])[0]
