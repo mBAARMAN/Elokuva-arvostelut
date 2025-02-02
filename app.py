07286ff6d152023d25cbe959f9a05b66fcdcb990
Baarman Mikael B <mbaarman@cubbli22-gold.local>
2025-02-02 17:31:56 +0200

    Add not found handling

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ def find_movie():
 @app.route("/movie/<int:movie_id>")
 def show_movie(movie_id):
     movie = movies.get_movie(movie_id)
+    if not movie:
+        abort(404)
     return render_template("show_movie.html", movie=movie)
 
 @app.route("/new_movie")
@@ -48,6 +50,8 @@ def create_movie():
 @app.route("/edit_movie/<int:movie_id>")
 def edit_movie(movie_id):
     movie = movies.get_movie(movie_id)
+    if not movie:
+        abort(404)
     if movie["user_id"] != session["user_id"]:
         abort(403)
     return render_template("edit_movie.html", movie=movie)
@@ -56,6 +60,8 @@ def edit_movie(movie_id):
 def update_movie():
     movie_id = request.form["movie_id"]
     movie = movies.get_movie(movie_id)
+    if not movie:
+        abort(404)
     if movie["user_id"] != session["user_id"]:
         abort(403)
 
@@ -72,6 +78,8 @@ def update_movie():
 def remove_movie(movie_id):
 
     movie = movies.get_movie(movie_id)
+    if not movie:
+        abort(404)
     if movie["user_id"] != session["user_id"]:
         abort(403)
 
